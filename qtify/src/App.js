361da014a838c0ba9703e4dced35fc0b7698c0e6
unknown <abhijeetpatole10@gmail.com>
2024-01-23 13:48:57 +0530

    update

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,63 +1,35 @@
-import axios from 'axios';
-import {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Navbar from './Components/Navbar';
-import HeroSection from './Components/HeroSection';
-import Section from './Components/Section';
-import FilterSection from './Components/FilterSection';
-import {useEffect} from 'react';
-import Accordian from './Components/Accordian';
+import Navbar from "./components/Navbar/Navbar"
 
+import { Outlet } from 'react-router-dom';
+import { StyledEngineProvider } from '@mui/material';
+import { fetchNewAlbums, fetchSongs, fetchTopAlbums } from './api/api';
 
-
-const ENDPOINT = 'https://qtify-backend-labs.crio.do/'
 
 function App() {
-  const [topAlbums, setTopAlbums] = useState([])
-  const [newAlbums, setNewAlbums] = useState([])
-  const [songs, setSongs] = useState([])
-  const [filteredSongs, setFilteredSongs] = useState([])
-  const [genres, setGenres] = useState([])
-
-
-  useEffect(() => {
-    axios.get(`${ENDPOINT}albums/top`)
-    .then(({data}) =>{
-       setTopAlbums(data)
+  const [data,setData]=useState({});
+  const generateData=(key,source)=>{
+    source().then((data) => {
+      setData((prevState) => {
+        return {...prevState,[key]:data};
+      })
     })
-    axios.get(`${ENDPOINT}albums/new`)
-    .then(({data}) =>{
-       setNewAlbums(data)
-    })
-    axios.get(`${ENDPOINT}songs`)
-    .then(({data}) =>{
-       setSongs(data)
-       setFilteredSongs(data)
-    })
-    axios.get(`${ENDPOINT}genres`)
-    .then(({data}) =>{
-      setGenres([{"key":"all","label":"All"}, ...data.data])
-    })
-  }, [])
+  }
+  useEffect(()=>{
+    generateData("topAlbums",fetchTopAlbums);
+    generateData("newAlbums",fetchNewAlbums);
+    generateData("songs",fetchSongs);
+  },[]);
+  const { topAlbums = [],newAlbums = [],songs=[] }=data;  
+  return (
+ <>
+ <StyledEngineProvider injectFirst >
+<Navbar/>
+<Outlet context={{data:{topAlbums,newAlbums,songs}}}/>
 
-  return ( <>
-   <Navbar />
-   <HeroSection />
-   <Section navId='ta' title='Top Albums' data={topAlbums}/>
-   <Section navId='na' title='New Albums' data={newAlbums}/>
-   <FilterSection title='Songs'  data={filteredSongs} filters={genres} 
-   executeFilter = {(genre) =>{
-    if(genre==='all'){
-      setFilteredSongs(songs)
-    } else{
-      setFilteredSongs(songs.filter(song => song.genre.key === genre))
-    }
-
-   }}/>
-
-    <Accordian/>
-    
-  </>
+</StyledEngineProvider>
+ </> 
   );
 }
 
